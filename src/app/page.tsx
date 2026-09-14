0c65ff4190a0713c74abe2d0f9954cbde5344365
Naongjin <syje1004@naver.com>
2026-09-14 15:33:34 +0900

    폴더 구조 재 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/ui/Button";
-import ProgressBar from "@/components/ui/Progess/ProgressBar";
-import ProgressRing from "@/components/ui/Progess/ProgressRing";
+import ProgressBar from "@/components/ui/ProgessBar";
+import ProgressRing from "@/components/ui/ProgressRing";
 
 export default function HomePage() {
   return (
