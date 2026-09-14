7990fedb4a0e63d41b3ade62a941651aac1356ef
Naongjin <syje1004@naver.com>
2026-09-14 16:40:08 +0900

    [Feat] goal card 레이아웃 배치 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Button from "@/components/ui/Button";
+import GoalCard from "@/components/ui/GoalCard";
 import ProgressBar from "@/components/ui/ProgessBar";
 import ProgressRing from "@/components/ui/ProgressRing";
 
 export default function HomePage() {
   return (
     <div>
-      <ProgressRing percentage={60} />
+      <GoalCard />
     </div>
   );
 }
---
+++ src/components/ui/GoalCard/index.tsx
@@ -0,0 +1,51 @@
+import FlagFilled from "@/assets/icons/Flag_filled.svg";
+import ProgressBar from "../ProgessBar";
+import Button from "../Button";
+
+{
+  /**
+    1. 아이콘 완성되면 추가하기
+    2. Badge 머지되면 교체하기
+    3. props 넘길 수 있게 하기
+*/
+}
+
+export default function GoalCard() {
+  return (
+    <div className="flex max-w-[400px] flex-col gap-20 rounded-[20px] bg-white p-24">
+      <div className="flex flex-col gap-12">
+        <div className="flex flex-row gap-12">
+          <div>dd</div>정보처리기사 합격하기<div>dd</div>
+        </div>
+
+        <ProgressBar percentage={64}></ProgressBar>
+        <div className="flex flex-row gap-12">
+          <div>ToDo</div>
+          <div>Done</div>
+        </div>
+      </div>
+      {/* 할 일 목록 */}
+      <div className="flex flex-col gap-8 py-8">
+        <div className="flex flex-row items-center gap-8">
+          <div className="h-12 w-12 bg-white"></div>
+          <p className="truncate">데이터베이스 기출풀기 데이터베이스 기출풀기 데이터베이스 기출풀기</p>
+        </div>
+        <div className="flex flex-row items-center gap-8">
+          <div className="h-12 w-12 bg-white"></div>
+          <p className="truncate">데이터베이스 기출풀기 데이터베이스 기출풀기 데이터베이스 기출풀기</p>
+        </div>
+        <div className="flex flex-row items-center gap-8">
+          <div className="h-12 w-12 bg-white"></div>
+          <p className="truncate">데이터베이스 기출풀기 데이터베이스 기출풀기 데이터베이스 기출풀기</p>
+        </div>
+      </div>
+      <Button
+        variant={"outline"}
+        size={"lg"}
+        className="border-primary-500 text-primary-600 w-full bg-white"
+      >
+        + 할 일 추가
+      </Button>
+    </div>
+  );
+}
