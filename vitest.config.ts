b6ced5fde1726be44184ac077160055af67a16ca
이원빈 <ldcumg@naver.com>
2026-09-14 11:57:22 +0900

    chore: 불필요한 코드 삭제

--- vitest.config.ts
+++ vitest.config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
         ],
         test: {
           name: "storybook",
-          setupFiles: [],
           browser: {
             enabled: true,
             headless: true,
