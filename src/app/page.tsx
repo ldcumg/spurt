bdace09fb7e0e4d58fc33121810ba9374a9c775b
Naongjin <syje1004@naver.com>
2026-09-15 11:57:08 +0900

    feat: mock데이터와 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Button from "@/components/ui/Button";
 import GoalCard from "@/components/ui/GoalCard";
+import { Goal, Todos } from "@/components/ui/GoalCard/mock";
 import ProgressBar from "@/components/ui/ProgessBar";
 import ProgressRing from "@/components/ui/ProgressRing";
 
 export default function HomePage() {
   return (
     <div>
-      <GoalCard />
+      <GoalCard
+        goal={Goal.goals[0]}
+        todo={Todos.todos[0]}
+      />
     </div>
   );
 }
--- src/components/ui/GoalCard/index.tsx
+++ src/components/ui/GoalCard/index.tsx
@@ -2,6 +2,9 @@ import { FlagFilled, More } from "@/assets/icons/index";
 import flagfilled from "@/assets/icons/Flag_filled.svg";
 import ProgressBar from "../ProgessBar";
 import Button from "../Button";
+import Badge from "../Badge";
+
+import { Goal, Todos } from "./mock";
 
 {
   /**
@@ -11,37 +14,53 @@ import Button from "../Button";
 */
 }
 
-export default function GoalCard() {
+interface GoalItem {
+  id: number;
+  title: string;
+  todoCount: number;
+  completedCount: number;
+}
+interface TodosItem {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+interface GoalCardProps {
+  goal: GoalItem;
+  todo: TodosItem;
+}
+
+export default function GoalCard({ goal, todo }: GoalCardProps) {
   return (
     <div className="flex max-w-[400px] flex-col gap-20 rounded-[20px] bg-white p-24">
       <div className="flex flex-col gap-12">
         <div className="flex justify-between">
           <div className="fles-row flex gap-12">
             <img src={FlagFilled} />
-            <p>정보처리기사 합격하기</p>
+            <p>{goal.title}</p>
           </div>
           <img src={More} />
         </div>
-
         <ProgressBar percentage={64}></ProgressBar>
         <div className="flex flex-row gap-12">
-          <div>ToDo</div>
-          <div>Done</div>
+          <Badge type="todo" />
+          <Badge type="done" />
         </div>
       </div>
       {/* 할 일 목록 */}
       <div className="flex flex-col gap-8 py-8">
         <div className="flex flex-row items-center gap-8">
           <div className="h-12 w-12 bg-white"></div>
-          <p className="truncate">데이터베이스 기출풀기 데이터베이스 기출풀기 데이터베이스 기출풀기</p>
+          <p className="truncate">{todo.title}</p>
         </div>
         <div className="flex flex-row items-center gap-8">
           <div className="h-12 w-12 bg-white"></div>
-          <p className="truncate">데이터베이스 기출풀기 데이터베이스 기출풀기 데이터베이스 기출풀기</p>
+          <p className="truncate">{todo.title}</p>
         </div>
         <div className="flex flex-row items-center gap-8">
           <div className="h-12 w-12 bg-white"></div>
-          <p className="truncate">데이터베이스 기출풀기 데이터베이스 기출풀기 데이터베이스 기출풀기</p>
+          <p className="truncate">{todo.title}</p>
         </div>
       </div>
       <Button
