69a6e853589e413bd8c9a439740fcb7838cc9c1a
wateralsie <minee1913@gmail.com>
2026-09-15 12:46:11 +0900

    style: 입력창 배경 흰색으로 설정

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -32,7 +32,7 @@ export default function Input({ label, error, id, ...props }: InputProps) {
       <div className="flex flex-col gap-4">
         <input
           id={inputId}
-          className={`text-body-lg h-48 w-full rounded-lg border px-16 outline-none disabled:opacity-50 ${borderClass}`}
+          className={`text-body-lg h-48 w-full rounded-lg border bg-white px-16 outline-none disabled:opacity-50 ${borderClass}`}
           {...props}
         />
         <p
