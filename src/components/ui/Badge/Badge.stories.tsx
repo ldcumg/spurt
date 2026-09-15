bb32cbe83a4a7e3d6262bd8c92b3086e8d089d3b
Naongjin <syje1004@naver.com>
2026-09-15 11:14:14 +0900

    feat: storybook Progress Ring 작성 완료

--- src/components/ui/Badge/Badge.stories.tsx
+++ src/components/ui/Badge/Badge.stories.tsx
@@ -6,9 +6,12 @@ const meta: Meta<typeof Badge> = {
   title: "Badge",
   component: Badge,
   tags: ["autodocs"],
+  parameters: {
+    //layout: "center",
+  },
   argTypes: {
     type: {
-      control: "select",
+      control: "radio",
       options: ["todo", "done", "goal", "category"],
       description: "뱃지 종류",
     },
@@ -18,6 +21,47 @@ const meta: Meta<typeof Badge> = {
 export default meta;
 
 type Story = StoryObj<typeof Badge>;
+
+export const Variants: Story = {
+  args: {
+    type: "todo",
+    className: "",
+  },
+  render: ({ type, className }) => (
+    <div className="flex justify-center gap-80">
+      {/* justify-center 사용할까 말까... */}
+      <div className="flex flex-col gap-10">
+        <p>Test</p>
+        <Badge
+          type={type}
+          className={className}
+        />
+      </div>
+
+      <div className="flex flex-col gap-10">
+        <p>OverView</p>
+        <div className="flex gap-10">
+          <Badge
+            type="todo"
+            className={className}
+          />
+          <Badge
+            type="done"
+            className={className}
+          />
+          <Badge
+            type="goal"
+            className={className}
+          />
+          <Badge
+            type="category"
+            className={className}
+          />
+        </div>
+      </div>
+    </div>
+  ),
+};
 
 export const Todo: Story = {
   args: {
