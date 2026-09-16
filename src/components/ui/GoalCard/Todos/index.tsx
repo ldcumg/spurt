353e95820897a49b90157ff89970f2da3a26ca0d
Naongjin <syje1004@naver.com>
2026-09-16 14:33:41 +0900

    feat: (in progress) todo 항목을 컴포넌트화

---
+++ src/components/ui/GoalCard/Todos/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState } from "react";
+import { Done, Todo } from "@/assets/icons/index";
+
+interface TodoListItemProps {
+  todo: TodoListItem;
+}
+
+export default function TodoListItem({ todo }: TodoListItemProps) {
+  const [isDone, setIsDone] = useState(todo.done);
+  return (
+    <div className="flex flex-row items-center gap-8">
+      {isDone ? (
+        <Done
+          onClick={() => setIsDone((prev) => !prev)}
+          className="cursor-pointer"
+        />
+      ) : (
+        <Todo
+          onClick={() => setIsDone((prev) => !prev)}
+          className="cursor-pointer"
+        />
+      )}
+      <p className="truncate">{todo.title}</p>
+    </div>
+  );
+}
