eb97d86b5ab9736234c0b84b1ade4a5b5d46d37a
gus8054 <gus8054@naver.com>
2026-09-16 18:35:27 +0900

    feat: 드롭박스 수정, 새목표 추가 기능

--- src/components/ui/Dropdown/index.tsx
+++ src/components/ui/Dropdown/index.tsx
@@ -1,83 +1,49 @@
 "use client";
 
-import { FlagFilled, Under } from "@/assets/icons";
-import { useEffect, useRef, useState } from "react";
-
-// 임시 svg
-// function TempIcon() {
-//   return (
-//     <svg
-//       viewBox="0 0 24 24"
-//       aria-hidden="true"
-//       className="size-[24px] fill-current stroke-current stroke-[1.8]"
-//     >
-//       <path
-//         d="M6 3v18M7 4h11l-2.6 4L18 12H7"
-//         strokeLinecap="round"
-//         strokeLinejoin="round"
-//       />
-//     </svg>
-//   );
-// }
-
-function ChevronDownIcon() {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      className="size-[24px] shrink-0 fill-none stroke-current stroke-2"
-    >
-      <path
-        d="m6 9 6 6 6-6"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-function CheckIcon() {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      className="size-[24px] shrink-0 fill-none stroke-current stroke-2"
-    >
-      <path
-        d="m5 12.5 4.2 4.2L19 7"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
+import { Done, FlagFilled, Plus, Under } from "@/assets/icons";
+import { SubmitEventHandler, useEffect, useRef, useState } from "react";
+import TextInput from "../TextInput";
 
 export type DropDownOption = {
   id: string;
   label: string;
 };
 
-type DropdownProps = {
+interface DropdownProps {
   options: DropDownOption[];
   value: string;
   placeholder?: string;
   disabled?: boolean;
   onChange: (option: DropDownOption) => void;
-};
+  onAddGoal: (value: string) => void;
+}
+
 const goals: DropDownOption[] = [
   { id: "javascript-service", label: "자바스크립트로 웹 서비스 만들기" },
   { id: "design-system", label: "디자인 시스템 강의 듣기" },
   { id: "portfolio", label: "프론트엔드 포트폴리오 완성하기" },
 ];
-
+/**
+ * 드롭다운 컴포넌트입니다.
+ * @param options DropDownOption 타입을 담고있는 리스트입니다. 예: [{id: string, label: string}, ...]
+ * @param value option 리스트 중에서 선택한 항목의 id 입니다.
+ * @param placeholder option 이 없을 때 나오는 문구입니다.
+ * @param disabled
+ * @param onChange 드롭다운 메뉴중 하나를 클릭했을 때 발생하는 이벤트 (arg : DropDownOption) => void
+ * @param onAddGoal 목표 생성 아이콘을 클릭했을 때 발생하는 이벤트, 매개변수는 목표 텍스트이다. (arg: string) => void
+ * @returns
+ */
 export default function Dropdown({
-  options = goals,
-  value = "portfolio",
+  options,
+  value,
   placeholder = "목표를 선택해 주세요",
   disabled = false,
   onChange,
+  onAddGoal,
 }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [inputGoalOpen, setInputGoalOpen] = useState(false);
+  const [goal, setGoal] = useState("");
   const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleOutsideClick = (event: MouseEvent) => {
@@ -96,6 +62,16 @@ export default function Dropdown({
   const handleSelect = (option: DropDownOption) => {
     onChange(option);
     setIsOpen(false);
+  };
+  const handleAddGoalClick = () => {
+    setInputGoalOpen(true);
+    setGoal("");
+  };
+  const handleAddGoalSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const goal = formData.get("goal");
+    onAddGoal(String(goal));
   };
   return (
     <div
@@ -142,42 +118,56 @@ export default function Dropdown({
             "text-title-xs font-bold",
           ].join(" ")}
         >
-          {options.length === 0 ? (
-            <li className="text-title-xs flex gap-[12px] rounded-xl p-8">
-              <FlagFilled />
-              등록된 목표가 없습니다.
+          {options.map((option) => {
+            const isSelected = option.id === value;
+
+            return (
+              <li
+                key={option.id}
+                onClick={() => handleSelect(option)}
+                className={[
+                  "text-title-xs",
+                  "cursor-pointer",
+                  isSelected ? "bg-primary-200" : "bg-transparent",
+                  "hover:bg-primary-200",
+                  "p-8",
+                  "flex items-center justify-between gap-[12px]",
+                  "rounded-lg",
+                ].join(" ")}
+              >
+                <span className="text-primary-600 grid size-[36px] shrink-0 place-items-center">
+                  <FlagFilled />
+                </span>
+                <span className="min-w-0 grow truncate">{option.label}</span>
+
+                {isSelected && (
+                  <span className="text-primary-600 grid size-[36px] shrink-0 place-items-center">
+                    <Done />
+                  </span>
+                )}
+              </li>
+            );
+          })}
+          {!inputGoalOpen ? (
+            <li
+              className="text-title-xs flex cursor-pointer justify-center gap-[12px] rounded-xl p-8"
+              onClick={handleAddGoalClick}
+            >
+              <Plus />새 목표 추가
             </li>
           ) : (
-            options.map((option) => {
-              const isSelected = option.id === value;
-
-              return (
-                <li
-                  key={option.id}
-                  onClick={() => handleSelect(option)}
-                  className={[
-                    "text-title-xs",
-                    "cursor-pointer",
-                    isSelected ? "bg-primary-200" : "bg-transparent",
-                    "hover:bg-primary-200",
-                    "p-8",
-                    "flex items-center justify-between gap-[12px]",
-                    "rounded-lg",
-                  ].join(" ")}
-                >
-                  <span className="text-primary-600 grid size-[36px] shrink-0 place-items-center">
-                    <FlagFilled />
-                  </span>
-                  <span className="min-w-0 grow truncate">{option.label}</span>
-
-                  {isSelected && (
-                    <span className="text-primary-600 grid size-[36px] shrink-0 place-items-center">
-                      <CheckIcon />
-                    </span>
-                  )}
-                </li>
-              );
-            })
+            <form
+              className="text-title-xs flex cursor-pointer items-center justify-center gap-[12px] rounded-xl p-8"
+              onSubmit={handleAddGoalSubmit}
+            >
+              <TextInput
+                name="goal"
+                placeholder="목표를 입력해주세요"
+              />
+              <button type="submit">
+                <Plus />
+              </button>
+            </form>
           )}
         </ul>
       )}
