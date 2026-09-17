f8ff13c626e6de4f2342fed8d69794b786a27bc8
이원빈 <ldcumg@naver.com>
2026-09-17 17:18:45 +0900

    chore: 임시 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-
-import Modal from "@/components/ui/Modal";
-import ModalActions from "@/components/ui/Modal/ModalActions";
-import { useDisclosure } from "@/hooks/disclosure/useDisclosure";
-
 export default function HomePage() {
-  const { close } = useDisclosure();
-  return (
-    <Modal
-      onClose={close}
-      title="dsfdsaf"
-      isOpen={true}
-    >
-      dsfdaf
-      <ModalActions
-        onClose={close}
-        onConfirm={close}
-        closeButtonLabel="취소"
-        confirmButtonLabel="확인"
-        isConfirmDisabled={false}
-      />
-    </Modal>
-  );
+  return <div>HomePage</div>;
 }
