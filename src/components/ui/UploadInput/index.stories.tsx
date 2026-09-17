c003088bbeada26dcbd2ead6271131acdb0a0d2e
wateralsie <minee1913@gmail.com>
2026-09-17 10:32:38 +0900

    docs: UploadInput 스토리북 수정

--- src/components/ui/UploadInput/index.stories.tsx
+++ src/components/ui/UploadInput/index.stories.tsx
@@ -1,5 +1,6 @@
 import UploadInput from ".";
 import { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { fn } from "storybook/test";
 
 const meta = {
   title: "components/UploadInput",
@@ -8,30 +9,54 @@ const meta = {
     layout: "centered",
   },
   tags: ["autodocs"],
+  args: {
+    onFileChange: fn(),
+  },
+  argTypes: {
+    onFileChange: { control: false },
+  },
 } satisfies Meta<typeof UploadInput>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const AllStates: Story = {
+  parameters: {
+    controls: { disable: true },
+  },
+  render: (args) => (
+    <div className="flex w-md flex-col gap-32">
+      <UploadInput {...args} />
+      <UploadInput
+        {...args}
+        label="파일"
+      />
+      <UploadInput
+        {...args}
+        label="사진"
+        placeholder="사진을 업로드해주세요"
+      />
+      <UploadInput
+        {...args}
+        label="파일"
+        error="파일 크기는 5MB 이하로 업로드해주세요"
+      />
+    </div>
+  ),
+};
+
+export const Playground: Story = {
+  decorators: [
+    (Story) => (
+      <div className="w-md">
+        <Story />
+      </div>
+    ),
+  ],
   args: {
     label: "사진",
     placeholder: "사진을 업로드해주세요",
-    onFileChange: (file) => console.log(file),
+    error: "",
   },
 };
-
-export const NoLabel: Story = {
-  args: {
-    onFileChange: (file) => console.log(file),
-  }
-}
-
-export const Error: Story = {
-  args: {
-    label: "파일",
-    error: "파일 크기는 5MB를 넘길 수 없습니다",
-    onFileChange: (file) => console.log(file),
-  }
-}
