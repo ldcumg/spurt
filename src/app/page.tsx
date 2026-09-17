6bf113e2749b9a894c4abbcae55085577574d125
Naongjin <syje1004@naver.com>
2026-09-17 11:14:36 +0900

    feat: layout 배치 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import GoalCard from "@/components/ui/GoalCard";
 import { Goal, Todos } from "@/components/ui/GoalCard/mock";
 
 import ProgressRing from "@/components/ui/ProgressRing";
+import DashBoardPage from "@/pages/DashBoard/page";
 
 export default function HomePage() {
   return (
     <div>
-      <GoalCard
-        goal={Goal.goals[0]}
-        todos={Todos.todos}
-      />
+      <DashBoardPage />
     </div>
   );
 }
