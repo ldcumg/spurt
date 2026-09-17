c3ca1b5f71a0945f756b56197a05135563326dae
wateralsie <minee1913@gmail.com>
2026-09-18 04:17:45 +0900

    chore: 로고 Logo 컴포넌트로 변경

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -2,18 +2,15 @@ import ActionSection from "./ActionSection";
 import GoalSection from "./GoalSection";
 import NavSection from "./NavSection";
 import ProfileSection from "./ProfileSection";
-import LogoHorizontalWithTagline from "@/assets/images/logo/LogoHorizontalWithTagline.webp";
-import Image from "next/image";
+import Logo from "@/components/ui/Logo";
 
 export default function Sidebar() {
   return (
     <aside className="flex min-h-screen w-280 shrink-0 flex-col justify-between bg-white px-20 py-32 shadow-sm">
       <div className="flex flex-col gap-24">
-        <Image
-          src={LogoHorizontalWithTagline}
-          alt="Spurt"
-          height={60}
-          priority
+        <Logo
+          variant="horizontalWithTagline"
+          className="mr-auto h-56"
         />
         <NavSection />
         <hr className="border-neutral-200" />
