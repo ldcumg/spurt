8341bb86d1cb7e7f8ac1917b19056819a8e94ac2
Naongjin <syje1004@naver.com>
2026-09-18 14:44:19 +0900

    design: 성유진 맡은 페이지 아이콘 디자인 맞춤

--- src/components/ui/GoalCard/index.tsx
+++ src/components/ui/GoalCard/index.tsx
@@ -24,12 +24,12 @@ export default function GoalCard({ goal, todos = [] }: GoalCardProps) {
       <div className="flex flex-col gap-12">
         <div className="flex justify-between">
           <div className="flex min-w-0 flex-1 flex-row gap-12">
-            <FlagFilled className="shrink-0" />
+            <FlagFilled className="text-primary-600 h-24 w-24 shrink-0" />
             <p className="text-title-sm truncate pt-2 leading-none whitespace-nowrap text-neutral-700">
               {goal.title || "목표를 추가하세요."}
             </p>
           </div>
-          <More className="shrink-0" />
+          <More className="h-24 w-24 shrink-0" />
         </div>
         <ProgressBar
           doneCount={completedCount}
@@ -63,7 +63,7 @@ export default function GoalCard({ goal, todos = [] }: GoalCardProps) {
         <Button
           variant={"outline"}
           size={"lg"}
-          className="border-primary-500 flex h-48 w-full flex-row items-center justify-center gap-4 bg-white"
+          className="border-primary-500 flex h-48 w-full flex-row items-center justify-center gap-8 bg-white"
         >
           <Plus className="text-primary-600 h-16 w-16 shrink-0" />
           <p className="text-primary-600 text-title-xs truncate">할 일 추가</p>
