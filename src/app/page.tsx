12b3583f51c6a2afa83a9c9e28d1de73992ce922
Naongjin <syje1004@naver.com>
2026-09-18 16:09:56 +0900

    rebase 전 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import DashBoardPage from "@/pages/DashBoard/page";
+import GoalPage from "@/pages/GoalPage/page";
 
 export default function HomePage() {
   return (
     <div>
-      <DashBoardPage />
+      <GoalPage />
     </div>
   );
 }
