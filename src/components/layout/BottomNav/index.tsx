27eb33798e5f75c13d78705fc8c28f31d307b3a6
wateralsie <minee1913@gmail.com>
2026-09-18 14:08:15 +0900

    fix: 아이콘 prop으로 넘기는 곳 use client 추가

--- src/components/layout/BottomNav/index.tsx
+++ src/components/layout/BottomNav/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { NAV_ITEMS } from "../navItems";
 import NavItem from "./NavItem";
 
@@ -5,8 +7,11 @@ export default function BottomNav() {
   return (
     <nav className="fixed bottom-0 left-0 w-full bg-white pb-34 shadow-md">
       <ul className="flex items-center gap-8 px-8 py-4">
-        {NAV_ITEMS.map(item => (
-          <li key={item.href} className="flex flex-1">
+        {NAV_ITEMS.map((item) => (
+          <li
+            key={item.href}
+            className="flex flex-1"
+          >
             <NavItem {...item} />
           </li>
         ))}
