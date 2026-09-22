5577a9dfc3731a3fedc1978ebbf8472ce24e729b
Naongjin <syje1004@naver.com>
2026-09-22 15:08:22 +0900

    fix: mockdata 수정 후 스토리북 수정 사항 반영

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -87,7 +87,7 @@ export default function DashboardPage() {
             </div>
           </div>
           {/** 최근 등록한 할 일을 3개까지 보여줌
-           * - 최근 등록한 할 일이 없는 경우 방어 필요
+           * - 최근 등록한 할 일이 없는 경우 방어
            */}
           {Todos.todos ? (
             [...Todos.todos]
