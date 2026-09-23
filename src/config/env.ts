9ca04643141a7231bf3c0cebcf62509d5741da2f
wateralsie <minee1913@gmail.com>
2026-09-23 11:20:10 +0900

    chore: 환경변수 세팅

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,3 @@
 // api base url
-export const BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL!;
-export const TEAM_ID = "team3-spurt";
+export const BASE_URL: string = process.env.API_BASE_URL!;
+export const TEAM_ID: string = process.env.TEAM_ID!;
